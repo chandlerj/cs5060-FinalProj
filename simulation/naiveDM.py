692923ca3f3794efad7d509ce4d0a438776db8f5
Chandler J <cjustice2000@gmail.com>
2024-12-02 16:25:41 -0700

    began writing plotters for naiveDM

--- simulation/naiveDM.py
+++ simulation/naiveDM.py
@@ -1,11 +1,12 @@
 from decisionMaker import DecisionMaker
 from simspace import SimState
 from datetime import timedelta
+import matplotlib.pyplot as plt
 import typing
 class NaiveDM(DecisionMaker):
     def __init__(self, sim_state):
         super().__init__(sim_state)
-
+        self.charge_rate = [[] for bus in self.state.buses]
 
     def update_chargers(self, timesteps) -> None:
         self.__calc_charge_rates()
@@ -21,6 +22,7 @@ class NaiveDM(DecisionMaker):
                     bus = connector.connected_to
                     if bus.current_soc() >= bus.desired_soc:
                         connector.update_charge_rate(0)
+                        self.charge_rate[bus.id].append(0)
                     else:
                         hours_remaining = (bus.departure_time - right_now).seconds // 3600
                         soc_delta = bus.desired_soc - bus.current_soc()
@@ -31,4 +33,25 @@ class NaiveDM(DecisionMaker):
                         else:
                             charge_rate = ((charge_needed * bus.battery_capacity) / hours_remaining) * 1000
                             connector.update_charge_rate(charge_rate)
+
+
+    def plot_bus_charge_rates(self):
+        for i, row in enumerate(self.charge_rate):
+            plt.plot(row, label=f"bus {i}")
+        plt.xlabel("hour of charge session")
+        plt.ylabel("power to deliver (kWh)")
+        plt.legend()
+        plt.grid(True)
+        plt.show()
+    
+    def plot_total_charge_rate(self):
+        totals = [0 for _ in self.charge_rate[0]]
+        for charge_rate in self.charge_rate:
+            for i, element in enumerate(charge_rate):
+                totals[i] += element
+        plt.plot(totals)
+        plt.xlabel("hour of charge session")
+        plt.ylabel("total power delivered (kWh)")
+        plt.grid(True)
+        plt.show()
 
